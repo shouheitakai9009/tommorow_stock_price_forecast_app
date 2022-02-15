b440e6c287d1ffb8928d900abdb97d457965c065
Shouhei Takai <shouheitakai.me@gmail.com>
2022-02-15 10:42:11 +0900

    1日後だけでなく、n日後を予想できるように修正。ただしnの数が大きくなるほど正解率が大幅に下がったのでパラメータを微調整

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 株価を取得
-bikeou_instance = finance.StockPriceUtil(4689)
-data = bikeou_instance.get_stock_price(share.PERIOD_TYPE_YEAR, 5, share.FREQUENCY_TYPE_DAY, 1)
+after_n_day: int = 1 # n日後を指定する変数
+instance = finance.StockPriceUtil(1783) # 銘柄コード
+data = instance.get_stock_price(share.PERIOD_TYPE_YEAR, 5, share.FREQUENCY_TYPE_DAY, 1)
 
 # データフレームに変換
 df = pd.DataFrame(data)
 df = dataframe.initialize_columns(df)
-df[dataframe.T_CLOSE] = df[dataframe.CLOSE].shift(-1)
+df[dataframe.N_CLOSE] = df[dataframe.CLOSE].shift(-after_n_day)
 df[dataframe.VOLATILITY] = stock_price.volatility(df)
 df[dataframe.SMA_5] = stock_price.sma(df, 5)
 df[dataframe.SMA_25] = stock_price.sma(df, 25)
@@ -30,10 +31,10 @@ predict_data = df[len(df)-1:len(df)]
 df = df[0:len(df)-1] # 最後の行はpredictで使用するので除外
 
 # 説明変数と検証用データと目的変数を取得
-x: np.ndarray = df.drop(dataframe.T_CLOSE, axis=1).values
-predict_x: np.ndarray = predict_data.drop(dataframe.T_CLOSE, axis=1).values
-t: np.ndarray = df[dataframe.T_CLOSE].values
-x_train, x_test, t_train, t_test = train_test_split(x, t, test_size=0.3, random_state=0, shuffle=False)
+x: np.ndarray = df.drop(dataframe.N_CLOSE, axis=1).values
+predict_x: np.ndarray = predict_data.drop(dataframe.N_CLOSE, axis=1).values
+t: np.ndarray = df[dataframe.N_CLOSE].values
+x_train, x_test, t_train, t_test = train_test_split(x, t, test_size=0.2, random_state=0)
 
 # モデルの実装
 model = LinearRegression()
@@ -42,4 +43,4 @@ score = model.score(x_test, t_test)
 print(f'正解率は{np.round(score*100, decimals=2)}%でした。')
 
 y = model.predict(predict_x)
-print(f"次の日の株価は{y}円ですよ！")
+print(f"{after_n_day}日後の株価は{np.round(y,decimals=1)}円になりそうです。")
--- utils/dataframe.py
+++ utils/dataframe.py
@@ -3,7 +3,7 @@ import pandas as pd
 HIGH = "high" # 今日の高値
 LOW = "low" # 今日の安値
 CLOSE = "close" # 今日の終値
-T_CLOSE = "t_close" # 翌営業日の終値
+N_CLOSE = "n_close" # n日後の終値
 VOLATILITY = "volatility" # 価格変動の大きさ
 SMA_5 = "sma5" # 5日単純移動平均
 SMA_25 = "sma25" # 25日単純移動平均
@@ -15,7 +15,7 @@ DC_S_M = "dc_s_m" # デッドクロス（短期 < 中期）
 
 # Dataframeのカラム定義を初期化
 def initialize_columns(df: pd.DataFrame):
-  df[T_CLOSE] = 0
+  df[N_CLOSE] = 0
   df[VOLATILITY] = 0
   df[SMA_5] = 0
   df[SMA_25] = 0
